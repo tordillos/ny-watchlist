d6a2bf61899117a7bc0d112cabf51249ba9dbb37
Pedro Blázquez <tordillos2@gmail.com>
2025-07-05 10:11:04 +0200

    chore: convert to compile app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center text-white">
+    <View className="flex-1 items-center justify-center">
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
